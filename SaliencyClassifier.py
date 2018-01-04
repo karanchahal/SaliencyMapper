60ac4a04c990933398a5852bbb421d300ee8e9be
karanchahal <karanbleep@gmail.com>
2018-01-04 23:24:17 +0530

    Started UpSampler work

--- SaliencyClassifier.py
+++ SaliencyClassifier.py
@@ -20,24 +20,6 @@ class SaliencyClassifier(nn.Module):
         self.scaleX = nn.AvgPool2d(kernel_size=16)
         self.fc = nn.Linear(BASE*8,class_size)
         self._initialize_weights()
-    
-
-
-    def _make_layer(self,num_filters,filter_sizes):
-        
-        layers = []
-
-        for i,filter_size in enumerate(filter_sizes):
-            num_filter = num_filters[i]
-            layers.append(
-                nn.Conv2d(self.inplanes, num_filter,
-                          kernel_size=filter_size, bias=False)
-            )
-            self.inplanes = num_filter
-        
-        # layers.append(nn.MaxPool2d(kernel_size=pool_size, stride=2, padding=1))
-
-        return nn.Sequential(*layers)
 
     def forward(self,x):
 
@@ -54,8 +36,6 @@ class SaliencyClassifier(nn.Module):
         scale3 = self.scale3(scale2)
         print(scale3.size())
 
-        
-
         scaleX = self.scaleX(scale3)
         scaleX = scaleX.view(self.batch_size,-1)
 
@@ -65,6 +45,27 @@ class SaliencyClassifier(nn.Module):
         print(scaleC.size())
 
         return scale0,scale1,scale2,scale3,scaleX,scaleC
+    
+    def _make_layer(self,num_filters,filter_sizes,bn=True,activation=True):
+        
+        layers = []
+
+        for i,filter_size in enumerate(filter_sizes):
+            num_filter = num_filters[i]
+            layers.append(
+                nn.Conv2d(self.inplanes, num_filter,
+                          kernel_size=filter_size, bias=False)
+            )
+            if bn:
+                layers.append(nn.BatchNorm2d(num_filter, affine=affine))
+            if activation:
+                layers.append(nn.ReLU())
+
+            self.inplanes = num_filter
+        
+        # layers.append(nn.MaxPool2d(kernel_size=pool_size, stride=2, padding=1))
+
+        return nn.Sequential(*layers)
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -79,6 +80,27 @@ class SaliencyClassifier(nn.Module):
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
+
+
+class SaliencyModel(nn.Module):
+    def __init__(self,batch_size,class_size):
+        self.class_size = class_size
+        self.batch_size = batch_size
+
+        self.classifier = SaliencyClassifier(self.class_size,self.batch_size)
+        #fefe
+    
+    def forward(self,x):
+
+class UpSampler(nn.Module):
+    def __init__():
+        self.upsampler = upsample_block(in_channels=in_channels,
+                                        out_channels=out_channels,
+                                        kernel_size=3)
+        bottleneck_in_channels = passthrough_channels + out_channels
+        self.bottleneck = _make_layer(num_filters=[bottleneck_in_channels],filter_size=[3])
+    def forward(self,x,passthrough):
+        
 
 x = torch.autograd.Variable(torch.randn((1,3,32,32)))
 model = SaliencyClassifier(10,1)

699a7ae3406ebde0b661dc308ef94c64a0baa349
karanchahal <karanbleep@gmail.com>
2018-01-07 15:21:44 +0530

    Training of classifier

---
+++ train_classifier.py
@@ -0,0 +1,60 @@
+from SaliencyClassifier import SaliencyClassifier
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+
+model = SaliencyClassifier(self.class_size,self.batch_size)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.data[0]
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
--- utils.py
+++ utils.py
@@ -1,8 +1,61 @@
-def total_variation(mask):
+import torch
+from scipy import misc
+from torch.autograd import Variable
+def total_variation(masks,power=2,border_penalty=0.3):
     #todo
-def average_mask(mask):
+    x_loss = torch.sum((torch.abs(masks[:,:1:,:] - masks[:,:,:-1,:]))**power)
+    y_loss = torch.sum((torch.abs(masks[:,:,:,1:] - masks[:,:,:-1,:]))**power)
+
+    if border_penalty > 0:
+        border = float(border_penalty)*torch.sum(masks[:,:,-1,:]**power + masks[:,:,0,:]**power + masks[:,:,:,-1]**power + masks[:,:,:,0]**power)
+    else:
+        border = 0
+
+    
+    return (x_loss + y_loss + border) / float(power*masks.size(0)) # normalized for batch size
+
+def tensor_of_shape(x):
+    return torch.zeros(*x.size())
+
+def average_mask_loss(masks):
+    if(power != 1):
+        masks = (masks + 0.0005)**power # prevent nan derivative of sqrt at 0 is inf
+    
+    return torch.mean(masks)
+def theta(images,masks):
+    color_range = 0.66
+    images = images.clone()
+    n,c,_,_ = images.size()
+    alt = tensor_of_shape(images) # gets tensor of zeros
+
+    #color range
+    alt += torch.Tensor(n,c,1,1).uniform_(-color_range/2,color_range/2)
+
+    #gaussian probability
     #todo
-def theta():
-    #todo
+
+    alt = Variable(alt, requires_grad=False)
+
+    return (masks*images.detach()) + (1. - masks)*alt.detach()
 def classfier_loss():
     #todo
+    return 1
+
+def test():
+    from PIL import Image
+    import numpy as np
+    import os,pycat
+    im = Variable(torch.Tensor(np.expand_dims(np.transpose(np.array(Image.open(os.path.join(os.path.dirname(__file__), 'test3.jpg'))), (2, 0, 1)), 0)/255.*2-1.), requires_grad=False)
+    print('Original')
+    misc.imshow(im[0].data.numpy())
+    for pres in [1., 0.5, 0.1]:
+        print('Mask strength =', pres)
+        for e in range(5):
+            m = Variable(torch.Tensor(1, 3, im.size(2), im.size(3)).fill_(pres), requires_grad=True)
+            res = theta(im, m)
+            misc.imshow(res[0].data.numpy())
+    s = torch.sum(res)
+    s.backward()
+    print(torch.sum(m.grad))
+
+test()

2310054bc254249b1beb921e0e634e3bb08831d2
karanchahal <karanbleep@gmail.com>
2018-01-24 22:08:42 +0530

    Refactoring #1 .Saliency Model

--- model.py
+++ model.py
@@ -1,44 +1,74 @@
-import resnet
-from scipy import misc
+'''ResNet in PyTorch.
+For Pre-activation ResNet, see 'preact_resnet.py'.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 from torch.autograd import Variable
-from torchvision import datasets, models, transforms
-from PIL import Image
-
-model = resnet.resnet50()
 
 
-imsize = 256
-loader = transforms.Compose([
-     transforms.Scale(imsize),
-     transforms.CenterCrop(224),
-     transforms.ToTensor(),
-     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-     ])
+class Block(nn.Module):
+    expansion = 1
 
-def image_loader(image_name):
-    """load image, returns cuda tensor"""
-    image = Image.open(image_name)
-    image = loader(image).float()
-    image = Variable(image, requires_grad=False)
-    image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
-    return image #assumes that you're using GPU
+    def __init__(self, in_planes, planes, stride=1):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-image = image_loader('./test.jpg')
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
-out = model(image)
-#print(out)
-maxk = 3
-_, pred = out.topk(maxk, 1, True, True)
-pred = pred.t()
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-print(pred)
-correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-print(correct)
-_, preds = torch.max(out.data, 1)
-# preds = preds.numpy()
 
-# print(classes[preds[0]])
-# # print(preds)
+class Net(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(Net, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def classifier():
+    return Net(Block, [2,2,2,2])

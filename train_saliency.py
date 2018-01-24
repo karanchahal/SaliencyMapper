7e69a820ba2f42123014ed0b2a373dff35a51e93
karanchahal <karanbleep@gmail.com>
2018-01-24 22:30:05 +0530

    Training saliency pipeline, cifar 10 function added

--- train_saliency.py
+++ train_saliency.py
@@ -1,4 +1,4 @@
-from SaliencyClassifier import SaliencyClassifier
+
 
 import torch
 import torchvision
@@ -6,8 +6,9 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torch.optim as optim
 from torch.autograd import Variable
+from scipy import misc
 
-def save_checkpoint(state, filename='small.pth.tar'):
+def save_checkpoint(state, filename='sal.pth.tar'):
     torch.save(state, filename)
 
 def load_checkpoint(net,optimizer,filename='small.pth.tar'):
@@ -15,36 +16,41 @@ def load_checkpoint(net,optimizer,filename='small.pth.tar'):
     net.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
     return net,optimizer
+
+def cifar10():
     
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                            shuffle=False, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return trainloader,testloader,classes
 
 
 
-net = SaliencyClassifier(10,4)
+
+
+net = ResNet18()
+net = net.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
+optimizer = optim.Adam(net.parameters())
+loss_func = Loss(num_classes=10)
+for epoch in range(3):  # loop over the dataset multiple times
 
-net,optimizer = load_checkpoint(net,optimizer)
-
-for epoch in range(10):  # loop over the dataset multiple times
-
-
+    
     running_loss = 0.0
     running_corrects = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -52,54 +58,24 @@ for epoch in range(10):  # loop over the dataset multiple times
         inputs, labels = data
 
         # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
+        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        scale0,scale1,scale2,scale3,scaleX,outputs = net(inputs)
-        _, preds = torch.max(outputs.data, 1)
-
-        loss = criterion(outputs, labels)
+        mask,out = net(inputs,labels)
+       
+#         _, preds = torch.max(out.data, 1)
+#         loss = criterion(out,labels)
+        loss = loss_func.get(mask,inputs,labels,black_box_func)
+      
+#         running_corrects += torch.sum(preds == labels.data)
+        running_loss += loss.data[0]
+        if(i%100 == 0):
+          print('Epoch = %f , Loss = %f '%(epoch+1 , running_loss/(4*(i+1))) )
+       
         loss.backward()
         optimizer.step()
-        
-        # print statistics
-        running_corrects += torch.sum(preds == labels.data)
-        running_loss += loss.data[0]
-        print(i)
-        print('Epoch = %f , Accuracy = %f, Loss = %f '%(epoch+1 , running_corrects/(4*(i+1)), running_loss/(4*(i+1))) )
-        
-    # save_checkpoint({
-    #     'state_dict': net.state_dict(),
-    #     'optimizer' : optimizer.state_dict()
-    #     })
-
-
-# Testing 
-
-
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-
-
-scale0,scale1,scale2,scale3,scaleX,outputs = net(Variable(images))
-_, predicted = torch.max(outputs.data, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
-
-correct = 0
-total = 0
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-print('Finished Training')
+    
+    save_checkpoint(net,'saliency_model.tar')

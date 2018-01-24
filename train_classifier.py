4b72c496ddc8e460aa8a1decda258cbe30d2d6b9
karanchahal <karanbleep@gmail.com>
2018-01-24 23:06:13 +0530

    Classifier training

--- train_classifier.py
+++ train_classifier.py
@@ -1,5 +1,3 @@
-from SaliencyClassifier import SaliencyClassifier
-
 import torch
 import torchvision
 import torch.nn as nn
@@ -7,7 +5,9 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 from torch.autograd import Variable
 
-def save_checkpoint(state, filename='small.pth.tar'):
+from scipy import misc
+
+def save_checkpoint(state, filename='sal.pth.tar'):
     torch.save(state, filename)
 
 def load_checkpoint(net,optimizer,filename='small.pth.tar'):
@@ -35,16 +35,16 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 
 
-net = SaliencyClassifier(10,4)
+black_box_func = classifier()
+black_box_func = black_box_func.cuda()
+
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
+optimizer = optim.Adam(black_box_func.parameters())
 
-net,optimizer = load_checkpoint(net,optimizer)
+for epoch in range(6):  # loop over the dataset multiple times
 
-for epoch in range(10):  # loop over the dataset multiple times
-
-
+    
     running_loss = 0.0
     running_corrects = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -52,54 +52,23 @@ for epoch in range(10):  # loop over the dataset multiple times
         inputs, labels = data
 
         # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
+        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        scale0,scale1,scale2,scale3,scaleX,outputs = net(inputs)
-        _, preds = torch.max(outputs.data, 1)
-
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+        out = black_box_func(inputs)
         
-        # print statistics
+        
+        _, preds = torch.max(out.data, 1)
+        loss = criterion(out,labels)   
         running_corrects += torch.sum(preds == labels.data)
         running_loss += loss.data[0]
-        print(i)
-        print('Epoch = %f , Accuracy = %f, Loss = %f '%(epoch+1 , running_corrects/(4*(i+1)), running_loss/(4*(i+1))) )
-        
-    save_checkpoint({
-        'state_dict': net.state_dict(),
-        'optimizer' : optimizer.state_dict()
-        })
-
-
-# Testing 
-
-
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-
-
-scale0,scale1,scale2,scale3,scaleX,outputs = net(Variable(images))
-_, predicted = torch.max(outputs.data, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
-
-correct = 0
-total = 0
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-print('Finished Training')
+        if(i%100 == 0):
+          print('Epoch = %f , Accuracy = %f, Loss = %f '%(epoch+1 , running_corrects/(4*(i+1)), running_loss/(4*(i+1))) )
+       
+        loss.backward()
+        optimizer.step()
+    
+    save_checkpoint(black_box_func, filename='black_box_func.tar')
